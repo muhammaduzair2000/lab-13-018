0eccf282ec5b8997ffdd10ed299696c95a2cbee9
muhammaduzair2000 <122357257+muhammaduzair2000@users.noreply.github.com>
2023-01-10 15:43:48 +0500

    Add files via upload

---
+++ exception.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.*;
+public class exception {
+	 public static void main(String args[]) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter the integer a: ");
+		 int num1 = sc.nextInt();
+		 Scanner sc2 = new Scanner(System.in);
+		 System.out.println("Enter the integer b : ");
+		 int num2 = sc.nextInt();
+		 try {
+			 int Answer = num1/num2;
+			 System.out.println
+			 (Answer);	
+			 }
+		 catch (ArithmeticException e) {
+			 System.out.println("ArithmeticException");		 
+			 }
+		 }
+	 }		 
